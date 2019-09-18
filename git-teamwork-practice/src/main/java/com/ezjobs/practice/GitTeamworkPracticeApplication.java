0de68f353a293ba50ca2b4d49250b402843b6975
blacktokki <39031723+blacktokki@users.noreply.github.com>
2019-09-18 17:06:13 +0900

    init

---
+++ git-teamwork-practice/src/main/java/com/ezjobs/practice/GitTeamworkPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ezjobs.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTeamworkPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTeamworkPracticeApplication.class, args);
+	}
+
+}
