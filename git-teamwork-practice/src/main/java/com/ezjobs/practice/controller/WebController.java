076c2f216d11960e4018b86e210cbb2fbf0ec433
blacktokki <39031723+blacktokki@users.noreply.github.com>
2019-09-24 18:10:22 +0900

    컨트롤러+외부환경 설정

--- git-teamwork-practice/src/main/java/com/ezjobs/practice/controller/WebController.java
+++ git-teamwork-practice/src/main/java/com/ezjobs/practice/controller/WebController.java
@@ -1,10 +1,6 @@
 package com.ezjobs.practice.controller;
 
 import org.springframework.web.bind.annotation.*;
-
-
-//import javax.servlet.http.HttpServletRequest;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
 
