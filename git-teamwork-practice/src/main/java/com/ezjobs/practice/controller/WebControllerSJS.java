6ff3cb19a16c5b484c52e1c3b7a3af083cf8de6e
USER <USER@USER-OM4TBUBUJN>
2019-10-06 20:13:06 +0900

    진성

---
+++ git-teamwork-practice/src/main/java/com/ezjobs/practice/controller/WebControllerSJS.java
@@ -0,0 +1,15 @@
+package com.ezjobs.practice.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+
+@Controller
+public class WebControllerSJS {
+	
+	@GetMapping("templateSJS")// 사용자가 칠 주소명.
+	public String templateSJS(){
+		return "templateSJS";//jsp파일명 
+	}
+	
+}
